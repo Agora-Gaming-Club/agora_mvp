e7d3e69eeaaa1baf6dfd8291647cfbfc71d38cb5
santoine@nerdery.com <santoine@nerdery.com>
2023-12-06 16:33:26 -0500

    disable debug:

--- kernel/settings.py
+++ kernel/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-*qr-#1n!5qq@gg1xk7y4i$!iz*r)=6ezk7zmcxr8$a$zjs3u(l"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "18.226.170.241", "dev.agoragaming.gg", "dev.agoragaming.club"]
 
