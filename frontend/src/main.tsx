fec2138cb988f72e3e27c1fa9f6bb66774033538
santoine@nerdery.com <santoine@nerdery.com>
2023-12-08 09:25:25 -0500

    add alert

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 
+alert('hello?');
 document.addEventListener('DOMContentLoaded', () => {
   createInertiaApp({
     resolve: (name) => {
