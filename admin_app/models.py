5f54355cb5218be140e4bbc988f858469b309188
Tristan Royal <tristan.royal@nerdery.com>
2023-12-14 11:09:41 -0700

    adding an email on completing a dispute

--- admin_app/models.py
+++ admin_app/models.py
@@ -1,4 +1,8 @@
 """Proxy models for use in this app's admin page."""
+from django.db.models.signals import post_save, post_init
+from django.dispatch import receiver
+from django.contrib import messages
+
 from api.models import Wager, GameName, Platform, Term, Game
 
 
@@ -35,6 +39,20 @@ class WagerDisputeProxy(Wager):
         proxy = True
         verbose_name = "Wager Dispute"
         verbose_name_plural = "Wager Disputes"
+
+
+@receiver(post_init, sender=WagerDisputeProxy)
+def remember_previous_status(sender, instance, **kwargs):
+    instance.previous_status = instance.status
+
+
+@receiver(post_save, sender=WagerDisputeProxy)
+def send_email(sender, instance, created, **kwargs):
+    previous_status = instance.previous_status
+    if previous_status == Wager.DISPUTED and instance.status == Wager.COMPLETED:
+        if instance.winner:
+            print(f"sending email to {instance.winner}")
+    print(instance.previous_status, instance.status)
 
 
 class WagerPayoutProxy(Wager):
