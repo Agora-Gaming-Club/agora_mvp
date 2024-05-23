c2d71fa8660697936d90994afd71279259e24b82
Kyle <kylekraft1@gmail.com>
2024-05-23 14:18:37 -0400

    extra logging

--- frontend/src/Components/Partials/Challenge/RequireChallengePaymentPartial.tsx
+++ frontend/src/Components/Partials/Challenge/RequireChallengePaymentPartial.tsx
@@ -68,22 +68,13 @@ const RequireChallengePaymentPartial: React.FC<Props> = ({
       };
 
       const initializePaynote = () => {
+        console.log('Initializing Paynote');
         try {
           window.SeamlessChex.Paynote = new PAYNOTE(objRequestIframe);
           window.SeamlessChex.Paynote.render();
         } catch (error) {
           console.error('Error initializing SeamlessChex:', error);
         }
-        
-//         if (window.SeamlessChex && window.SeamlessChex.Paynote) {
-//           try {
-//             new window.SeamlessChex.Paynote(objRequestIframe).render();
-//           } catch (error) {
-//             console.error('Error initializing SeamlessChex:', error);
-//           }
-//         } else {
-//           console.error('SeamlessChex or SeamlessChex.Paynote is not available');
-//         }
       };
 
       initializePaynote();
